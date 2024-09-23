2c39c70effcf6ba3394cb0ef56341a53f5fcd036
sunwardodin <sunwardodin96@outlook.com>
2024-09-23 07:20:39 -0700

    somewhere

--- main.go
+++ main.go
@@ -5,69 +5,90 @@ import "fmt"
 func main() {
 
 	/*
-					// Array literal
-					a := [3]int{42, 43, 44}
-					fmt.Println(a)
+								// Array literal
+								a := [3]int{42, 43, 44}
+								fmt.Println(a)
 
-					b := [...]string{"Hello", "Gophers"}
-					fmt.Println(b)
+								b := [...]string{"Hello", "Gophers"}
+								fmt.Println(b)
 
-					var c [2]int
-					c[0] = 7
-					c[1] = 8
-					fmt.Println(c)
+								var c [2]int
+								c[0] = 7
+								c[1] = 8
+								fmt.Println(c)
 
-				// This is a slice
-				xs := []string{"hello", "world"}
-				for _, v := range xs {
-					fmt.Printf("%v\n", v)
-				}
-				fmt.Println(len(xs))
+							// This is a slice
+							xs := []string{"hello", "world"}
+							for _, v := range xs {
+								fmt.Printf("%v\n", v)
+							}
+							fmt.Println(len(xs))
+							fmt.Println("--------------------")
+
+						// This is appending to the slice
+						xs = append(xs, "!")
+						fmt.Println(xs)
+
+					// This is slicing a slice
+					xi := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+					fmt.Printf("xi - %#v\n", xi)
+					fmt.Println("--------------------")
+
+					// [inclusive:exclusive]
+					fmt.Printf("xi - %#v\n", xi[1:5])
+					fmt.Println("--------------------")
+
+					// [inclusive:]
+					fmt.Printf("xi - %#v\n", xi[2:])
+					fmt.Println("--------------------")
+
+					// [:exclusive]
+					fmt.Printf("xi - %#v\n", xi[:5])
+					fmt.Println("--------------------")
+
+				// Deleting from a slice
+				xi := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+				fmt.Printf("xi - %#v\n", xi)
 				fmt.Println("--------------------")
 
-			// This is appending to the slice
-			xs = append(xs, "!")
-			fmt.Println(xs)
+				xi = append(xi[:4], xi[5:]...)
+				fmt.Printf("xi - %#v\n", xi)
+				fmt.Println("--------------------")
 
-		// This is slicing a slice
-		xi := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
-		fmt.Printf("xi - %#v\n", xi)
-		fmt.Println("--------------------")
+			// make function
+			xi2 := make([]int, 0, 10)
+			fmt.Println(xi2)
+			fmt.Println(len(xi2))
+			fmt.Println(cap(xi2))
+			xi2 = append(xi2, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+			fmt.Println(xi2)
+			fmt.Println("--------------------")
+			xi2 = append(xi2, 10, 11, 12, 13)
+			fmt.Println(xi2)
+			fmt.Println(len(xi2))
+			fmt.Println(cap(xi2))
 
-		// [inclusive:exclusive]
-		fmt.Printf("xi - %#v\n", xi[1:5])
-		fmt.Println("--------------------")
+		// Multi dimensional slice
+		jb := []string{"J", "B", "M", "C"}
+		jm := []string{"M", "P", "G", "V"}
+		fmt.Println(jb, jm)
 
-		// [inclusive:]
-		fmt.Printf("xi - %#v\n", xi[2:])
-		fmt.Println("--------------------")
-
-		// [:exclusive]
-		fmt.Printf("xi - %#v\n", xi[:5])
-		fmt.Println("--------------------")
+		xxs := [][]string{jb, jm}
+		fmt.Println(xxs)
 	*/
 
-	// Deleting from a slice
-	xi := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
-	fmt.Printf("xi - %#v\n", xi)
-	fmt.Println("--------------------")
+	a := []int{0, 1, 2, 3, 4, 5}
+	b := a
 
-	xi = append(xi[:4], xi[5:]...)
-	fmt.Printf("xi - %#v\n", xi)
-	fmt.Println("--------------------")
+	fmt.Println("a ", a)
+	fmt.Println("b ", b)
+	fmt.Println("---------------")
 
-	// make function
-	xi2 := make([]int, 0, 10)
-	fmt.Println(xi2)
-	fmt.Println(len(xi2))
-	fmt.Println(cap(xi2))
-	xi2 = append(xi2, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
-	fmt.Println(xi2)
-	fmt.Println("--------------------")
-	xi2 = append(xi2, 10, 11, 12, 13)
-	fmt.Println(xi2)
-	fmt.Println(len(xi2))
-	fmt.Println(cap(xi2))
+	a[0] = 7
+
+	fmt.Println("a ", a)
+	fmt.Println("b ", b)
+	fmt.Println("---------------")
 
 	/*
 		{
